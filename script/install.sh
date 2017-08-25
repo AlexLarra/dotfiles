8a9bfd3a5553adb872a4acfe8fc25756669a9d36
AlexLarra <clausrybnic@openmailbox.org>
2017-08-25 15:34:49 +0200

    Adapt dotfiles to Archlinux

--- script/install.sh
+++ script/install.sh
@@ -8,6 +8,10 @@ function install {
   ln -sf "$dotfiles_dir/bash/bash_aliases" ~/.bash_aliases
   ln -sf "$dotfiles_dir/bash/profile" ~/.profile
 
+  # provide __git_ps1 in Arch
+  echo 'source /usr/share/git/completion/git-prompt.sh' >> ~/.bashrc
+
+  load_aliases_in_bashrc
   load_profile_in_bashrc
 
   source ~/.bashrc
@@ -15,9 +19,15 @@ function install {
   echo "Installation completed. Enjoy!"
 }
 
-# non-login shell does not load .profile
+# non-login shell does not load .bash_profile
 function load_profile_in_bashrc {
   LINE='if [ -f ~/.profile ]; then . ~/.profile ; fi'
+  FILE=~/.bashrc
+  append_a_line_if_not_exists "$LINE" "$FILE"
+}
+
+function load_aliases_in_bashrc {
+  LINE='if [ -f ~/.bash_aliases ]; then . ~/.bash_aliases ; fi'
   FILE=~/.bashrc
   append_a_line_if_not_exists "$LINE" "$FILE"
 }
