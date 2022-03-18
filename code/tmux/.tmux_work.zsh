6da62bccd72853a6499e37b93200cc80346a49c4
AlexLarra <alexander.larrazabal@crisalix.com>
2022-03-18 12:41:17 +0100

    add alias myip

--- code/tmux/.tmux_work.zsh
+++ code/tmux/.tmux_work.zsh
@@ -65,6 +65,9 @@ tmux select-pane -t 2
 tmux send-keys -t work 'cmus' c-m
 tmux select-pane -t 3
 tmux send-keys -t work 'neofetch --cpu_temp "C" --color_blocks "off"' c-m
+tmux select-pane -t 4
+tmux send-keys -t work 'clear' c-m
+tmux send-keys -t work 'myip' c-m
 
 tmux select-window -t work:1
 tmux attach -t work
